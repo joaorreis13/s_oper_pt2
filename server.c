7fd56680fcde6bb76c22a9afde59c239d570a547
joaorreis <joaorreis@gmail.com>
2018-04-29 23:19:04 +0100

    update

---
+++ server.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <time.h>
+
+//Funcao que inicializa o server
+int startserver(char *seats, char *n_tick_off, char *open_time){
+
+
+}
+
+int main(int agrc,char *argv[]){
+    
+    startserver(argv[1],argv[2],argv[3]);
+
+
+    return 0;
+}
