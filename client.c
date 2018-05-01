ccd8805d640566b9c1fc3ac70acddc7b99657bb5
joaorreis <joaorreis@gmail.com>
2018-05-01 17:29:16 +0100

    to-dos and fifo pseudo creation added

--- client.c
+++ client.c
@@ -5,7 +5,7 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <time.h>
@@ -13,16 +13,41 @@
 #define WIDTH_SEAT = 4
 #define WIDTH_PID = 5
 
-//Function that initializes the client
-int client_init(char *time_out, char *num_wanted_seats, char *pref_seat_list){
+//TO-DOS
 
+//escrita em clog.txt
+
+
+//Function that initializes the client
+int client_init(char *time_out, char *num_wanted_seats, char *pref_seat_list)
+{
 
 }
+//FIFO que recebe os pedidos de reserva de lugar
+int create_fifo_request_write()
+{	
+	//FIFO deve ter o nome ansXXXXX, em que XXXXX representa o PID do cliente (TO-DO)
+	int fd;
+	char *request = "/tmp/request";
 
-int main(int agrc,char *argv[]){
-    
-    client_init(argv[1],argv[2],argv[3]);
+	// create the FIFO (named pipe)
+	mkfifo(request, 0666);
 
+	// Write something to fifo
+	fd = open(request, O_WRONLY);
+	write(fd, "Hi", sizeof("Hi"));
+	close(fd);
 
-    return 0;
+	// remove the FIFO
+	unlink(request);
+
+	return 0;
+}
+
+int main(int agrc, char *argv[])
+{
+
+	client_init(argv[1], argv[2], argv[3]);
+
+	return 0;
 }
--- server.c
+++ server.c
@@ -5,26 +5,58 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <time.h>
+
+
+//TO-DOS
+
+//CRIAR MAIN THREAD
+//CRIAR THREADS num_ticket_offices (geradas por thread main)
+//int isSeatFree(Seat *seats, int seatNum) – testa se o lugar seatNum esta livre
+/*void bookSeat(Seat *seats, int seatNum, int clientId) – reserva o lugar seatNum
+para o cliente cujo identificador e clientId */
+/*void freeSeat(Seat *seats, int seatNum) – liberta o lugar seatNum, 
+apoos uma tentativa de reserva sem sucesso, 
+em que foram pre-reservados alguns lugares mas nao 
+foi possivel reservar todos os lugares pretendidos.*/
+//Escrita em slog.txt
 
 //Defines
 
 #define MAX_ROOM_SEATS = 140
 #define MAX_CLI_SEATS = 100
-#define WIDTH_SEAT = 4 
-//Function that initializes the server
-int startserver(char *num_room_seats, char *num_ticket_offices, char *open_time){
+#define WIDTH_SEAT = 4
+#define MAX_BUF 1024
 
+//Function that initializes the server
+int startserver(char *num_room_seats, char *num_ticket_offices, char *open_time)
+{
 
 }
 
-int main(int agrc,char *argv[]){
-    
-    startserver(argv[1],argv[2],argv[3]);
+//FIFO que recebe os pedidos de reserva de lugar
+int create_fifo_request_read()
+{
+	int fd;
+	char *request = "/tmp/request";
+	char buf[MAX_BUF];
 
+	// open, read, and display the message from the FIFO
+	fd = open(request, O_RDONLY);
+	read(fd, buf, MAX_BUF);
+	printf("Received: %s\n", buf);
+	close(fd);
 
-    return 0;
+	return 0;
+}
+
+int main(int agrc, char *argv[])
+{
+
+	startserver(argv[1], argv[2], argv[3]);
+
+	return 0;
 }
